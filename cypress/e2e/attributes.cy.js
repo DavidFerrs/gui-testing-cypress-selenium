08802397b2bc93d67b3b695e6b7fbcf0b993c278
David Ferreira <81036027+DavidFerrs@users.noreply.github.com>
2024-04-25 23:32:51 -0300

    cypress tests

--- cypress/e2e/attributes.cy.js
+++ cypress/e2e/attributes.cy.js
@@ -5,8 +5,29 @@ describe('attributes', () => {
     cy.get('[id="_password"]').type('sylius');
     cy.get('.primary').click();
   });
-  // Remove .only and implement others test cases!
-  it.only('testing edit attribute position', () => {
+
+  function createAttribute(codeAtt, nameAtt, posAtt) {
+    cy.clickInFirst('a[href="/admin/product-attributes/"]');
+    cy.get('*[class^="ui labeled icon top right floating dropdown button primary link"]').click();
+    cy.clickInFirst('a[href="/admin/product-attributes/text/new"]');
+
+    if (codeAtt != '') {
+      cy.get('*[id="sylius_product_attribute_code"]').type(codeAtt);
+    }
+
+    if (posAtt != '') {
+      cy.get('*[id="sylius_product_attribute_position"]').type(posAtt);
+    }
+
+    if (nameAtt != '') {
+      cy.get('*[id="sylius_product_attribute_translations_en_US_name"]').type(nameAtt);
+    }
+
+    cy.get('*[class^="ui labeled icon primary button"]').click();
+
+  }
+
+  it('testing edit attribute position', () => {
     // Click in products in side menu
     cy.clickInFirst('a[href="/admin/product-attributes/"]');
     // Type in value input to search for specify attribute
@@ -23,12 +44,78 @@ describe('attributes', () => {
     // Assert that attribute has been updated
     cy.get('body').should('contain', 'Product attribute has been successfully updated.');
   });
-  it('test case 2', () => {
-    // Implement your test case 2 code here
-  });
-  it('test case 3', () => {
-    // Implement your test case 3 code here
+
+  it('Criar atributo novo', () => {
+    createAttribute('silk-fabric-type-new', 'silk fabric type new', '2');
+    cy.get('body').should('contain', 'Product attribute has been successfully created.');
   });
 
-  // Implement the remaining test cases in a similar manner
+  it('Criar atributo novo com código de atributo já existente: erro', () => {
+
+    createAttribute('silk-fabric-type-dupped', 'silk fabric type dupped', '3');
+    createAttribute('silk-fabric-type-dupped', 'silk fabric type dupped', '3');
+
+    cy.get('body').should('contain', 'This form contains errors.');
+    cy.get('body').should('contain', 'This code is already in use.');
+  });
+
+  it('Filtrar atributo que existe', () => {
+    createAttribute('silk-fabric-type-filter', 'silk fabric type filter', '4');
+
+    cy.clickInFirst('a[href="/admin/product-attributes/"]');
+    cy.get('*[id="criteria_code_value"]').type('silk-fabric-type-filter');
+    cy.get('*[class^="ui blue labeled icon button"]').click();
+
+    cy.get('body').should('contain', 'silk-fabric-type-filter');
+
+  });
+
+  it('Filtrar atributo que não existe', () => {
+    cy.clickInFirst('a[href="/admin/product-attributes/"]');
+    cy.get('*[id="criteria_code_value"]').type('silk-fabric-type-filter-non-existent');
+    cy.get('*[class^="ui blue labeled icon button"]').click();
+
+    cy.get('body').should('contain', 'There are no results to display');
+
+  });
+
+  it('Excluir atributo existente', () => {
+
+    createAttribute('silk-fabric-type-delete', 'silk fabric type delete', '5');
+
+    cy.clickInFirst('a[href="/admin/product-attributes/"]');
+    cy.get('*[class^="ui red labeled icon button"]').eq(5).click();
+    cy.get('*[class^="ui green ok inverted button"]').click();
+
+    cy.get('body').should('contain', 'Product attribute has been successfully deleted.');
+  });
+
+
+  it('Tentar criar atributo sem nome: erro', () => {
+
+    createAttribute('silk-fabric-type-no-name', '', '6');
+    cy.get('body').should('contain', 'This form contains errors.');
+
+  });
+
+  it('Tentar criar atributo sem código: erro', () => {
+
+    createAttribute('', 'silk fabric type no code', '7');
+    cy.get('body').should('contain', 'This form contains errors.');
+
+  });
+
+  it('Tentar criar atributo sem posição: sucesso, pos automática', () => {
+
+    createAttribute('silk-fabric-type-no-pos', 'silk fabric type no pos', '');
+    cy.get('body').should('contain', 'Product attribute has been successfully created.');
+
+  });
+
+  it('Tentar criar atributo passando posição negativa', () => {
+
+    createAttribute('silk-fabric-type-neg-pos', 'silk fabric type neg pos', '-1');
+    cy.get('body').should('contain', 'Product attribute has been successfully created.');
+
+  });
 });
